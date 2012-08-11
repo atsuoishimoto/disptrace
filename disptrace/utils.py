ca9624cb8102e5f2329d5c327f83c9d6bc2c290e
ishimoto <ishimoto@ubuntu-tp200.(none)>
2012-08-11 15:01:04 +0900

    Some refactoring.

--- disptrace/utils.py
+++ disptrace/utils.py
@@ -1,3 +1,5 @@
+import ast 
+
 class MementoMetaclass(type):
     """
     Classes that use this caching metaclass will have their instances
@@ -16,3 +18,20 @@ class MementoMetaclass(type):
             instance = type.__call__(self, *args)
             self.cache[key] = instance
             return instance
+ 
+class CodeManager(object): 
+    """ 
+    Holder for compiled Python modules. Instance creation highly cached. 
+    """ 
+    __metaclass__ = MementoMetaclass 
+ 
+    def __init__(self, filepath): 
+        self.filepath = filepath 
+        self.source = open(filepath).read() 
+        self.sourcelines = [ None ] + self.source.splitlines() 
+        try: 
+            self.ast = ast.parse(self.source) 
+        except StandardError: 
+            self.ast = None 
+
+
